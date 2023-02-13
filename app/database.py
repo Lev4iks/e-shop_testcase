73175d7d5cf8aac3868c43b33dfc9253c2c29ca3
Lev Sokolov <lev4egs@gmail.com>
2023-02-13 14:55:32 +0300

    removed drop all data operation in models initialisation

--- app/database.py
+++ app/database.py
@@ -28,7 +28,6 @@ async def get_db_session() -> Generator:
 
 async def init_models():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     logger.info("Initialized database models")
 
